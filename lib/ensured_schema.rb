17ea23f4853effb75c1d57c589614e121fc83a40
Chris Conley <chris.m.conley@gmail.com>
2010-12-15 17:28:12 -0500

    removed commented table_exists

--- lib/ensured_schema.rb
+++ lib/ensured_schema.rb
@@ -4,13 +4,3 @@ require 'ensured_schema/schema_statements'
 require 'ensured_schema/table'
 require 'ensured_schema/ensured_table'
 require 'ensured_schema/schema'
-
-# module ActiveRecord
-#   module ConnectionAdapters
-#     class AbstractAdapter
-#       def table_exists?(table_name)
-#         tables.map(&:downcase).include?(table_name.to_s.downcase)
-#       end
-#     end
-#   end
-# end
